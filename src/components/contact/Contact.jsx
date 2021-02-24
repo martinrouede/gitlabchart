8961460e74cadc05f5d638cd373af57bafdcbbfc
Martin Rouede <martinrouede1@gmail.com>
2021-02-24 18:58:18 -0300

    refactoring del manejo del usuario loggeado

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import Styles from './Styles';
@@ -14,9 +13,6 @@ const Contact = (props) => {
 
     return (
         <div className={classes.infoContact}>
-            <Link href='https://twitter.com/martinrouede'>
-                <FontAwesomeIcon icon={faTwitter} className={classes.icon} />
-            </Link>
             <Link href='https://github.com/martinrouede'>
                 <FontAwesomeIcon icon={faGithub} className={classes.icon} />
             </Link>
